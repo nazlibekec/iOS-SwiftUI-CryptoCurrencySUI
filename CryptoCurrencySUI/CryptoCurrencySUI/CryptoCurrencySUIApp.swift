45ef3c8616b4d64a479e1f4cdfb6e0e65bcb48a2
nazlibekec <nazlibekec@gmail.com>
2023-04-19 12:51:10 +0300

    First commit

---
+++ CryptoCurrencySUI/CryptoCurrencySUI/CryptoCurrencySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencySUIApp.swift
+//  CryptoCurrencySUI
+//
+//  Created by Nazlı Bekeç on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
