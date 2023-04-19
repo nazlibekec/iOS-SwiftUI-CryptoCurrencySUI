dd38f51da6823905558b8efd341e7aa85e6280cb
nazlibekec <nazlibekec@gmail.com>
2023-04-19 15:35:10 +0300

    webservice Alamofire kullanılarak tekrar yazıldı.

--- CryptoCurrencySUI/CryptoCurrencySUI/Service/Webservice.swift
+++ CryptoCurrencySUI/CryptoCurrencySUI/Service/Webservice.swift
@@ -9,30 +9,63 @@ import Foundation
 import Alamofire
 
 class CryptoDataService {
-    
-    func downloadCurrencies (url : URL, completion: @escaping (Result<[CryptoCurrency]?, DownloaderError>) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            if let error = error {
-                print(error.localizedDescription)
-                completion(.failure(.badURL))
+
+    func downloadCurrencies(url: URL, completion: @escaping (Result<[CryptoCurrency]?, DownloaderError>) -> Void) {
+        AF.request(url).validate().responseDecodable(of: [CryptoCurrency].self) { response in
+            switch response.result {
+            case .success(let currencies):
+                completion(.success(currencies))
+            case .failure(let error):
+                switch error {
+                case .invalidURL:
+                    completion(.failure(.badURL))
+                case .responseValidationFailed(let reason):
+                    switch reason {
+                    case .dataFileNil, .dataFileReadFailed:
+                        completion(.failure(.noData))
+                    default:
+                        completion(.failure(.dataParseError))
+                    }
+                default:
+                    completion(.failure(.noData))
+                }
             }
-            guard let data = data, error == nil else {
-                return completion(.failure(.noData))
-            }
-            guard let currencies = try? JSONDecoder().decode([CryptoCurrency].self, from: data) else {
-                    return completion(.failure(.dataParseError))
-            }
-                                  
-            completion(.success(currencies))
-            }.resume()
+        }
     }
     
-
-    
-    enum DownloaderError : Error {
+    enum DownloaderError: Error {
         case badURL
         case noData
         case dataParseError
     }
 }
+
+/*
+ 
+URLSession ile yazılan servis
+ 
+ import Foundation
+ class CryptoDataService {
+     func downloadCurrencies (url : URL, completion: @escaping (Result<[CryptoCurrency]?, DownloaderError>) -> Void) {
+         URLSession.shared.dataTask(with: url) { data, response, error in
+             if let error = error {
+                 print(error.localizedDescription)
+                 completion(.failure(.badURL))
+             }
+             guard let data = data, error == nil else {
+                 return completion(.failure(.noData))
+             }
+             guard let currencies = try? JSONDecoder().decode([CryptoCurrency].self, from: data) else {
+                     return completion(.failure(.dataParseError))
+             }
+             completion(.success(currencies))
+             }.resume()
+     }
+     enum DownloaderError : Error {
+         case badURL
+         case noData
+         case dataParseError
+     }
+ }
+ */
+
